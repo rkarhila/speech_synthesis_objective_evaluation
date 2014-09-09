311d8948d11fe9675958bdc5120961bff698e161
rkarhila <rkarhila@iki.fi>
2014-09-09 17:54:23 +0300

    drawing plots, again and again

--- draw_plots_one_at_a_time.m
+++ draw_plots_one_at_a_time.m
@@ -1,233 +1,4 @@
 
-local_conf;
-
-% Figurecounter
-fc=100;
-
-% How many tests do we do?
-testcount=length(mapmethods)^2 + length(gaussmethods)*length(gausscomps);
-% Make a list of file pair distances, initialise to zero:
-testlist=cell(testcount,1);
-ind=1
-for y=1:length(mapmethods)
-spec_and_distmethod=mapmethods{y};
-mapspec=spec_and_distmethod{1};
-mapdist=spec_and_distmethod{2};
-for z=1:length(mapmethods)
-spec_and_distmethod=mapmethods{z};
-pathspec=spec_and_distmethod{1};
-pathdist=spec_and_distmethod{2};
-testlist{ind} = ...
-['map: ',mapspec,'-',mapdist,', path:',pathspec,'-',pathdist];
-ind=ind+1;
-end
-end
-for y=1:length(gaussmethods)
-spec_and_distmethod=gaussmethods{y};
-featspec=spec_and_distmethod{1};
-featdist=spec_and_distmethod{2};
-for j0=1:length(gausstypes)
-for z=1:length(gausscomps{j0})
-testlist{ind} =...
-['feat: ',featspec,'-',featdist,', ',gausstypes{j0},' covariance, gausscomp:',num2str(gausscomps{j0}(z))];
-ind=ind+1;
-end
-end
-end
-testlist{ind}= 'PESQ Narrowband MOS';
-ind=ind+1;
-testlist{ind}= 'PESQ Narrowband MOS LQ0';
-ind=ind+1;
-testlist{ind}= 'PESQ Wideband MOS LQ0';
-
-disp(testlist)
-
-
-if 1 == 0
-  evaluate_test_2008;
-  evaluate_test_2009;
-  evaluate_test_2010;
-  evaluate_test_2011;
-  evaluate_test_2012;
-  evaluate_test_2013;
-end
-
-
-if 1 == 0
-%
-%
-%   Similarity and naturalness for all test data.
-%
-%
-
-tests={tests2008, tests2009, tests2010, tests2011, tests2012,  tests2013};
-
-sim=tests{1}{1}.scores(:,7);
-nat=tests{1}{2}.scores(:,7);
-
-n=1;
-for m=3:length(tests{n})
-  if tests{n}{m}.testtype == 'nat'
-      nat=[nat,tests{n}{m}.scores(:,7)];
-  else
-      sim=[sim,tests{n}{m}.scores(:,7)];
-  end
-end
-
-for n=2:length(tests)
-   for m=1:length(tests{n})
-      if tests{n}{m}.testtype == 'nat'
-          nat=[nat,tests{n}{m}.scores(:,7)];
-      else
-          sim=[sim,tests{n}{m}.scores(:,7)];
-      end
-   end
-end
-
-% 
-% figure
-% subplot(2,1,1)
-% %errorbar(mean(sim'),std(sim'),'o')
-% boxplot(sim')
-% title('similarity')
-% grid on
-% subplot(2,1,2)
-% %errorbar(mean(nat'),std(nat'),'o')
-% boxplot(nat')
-% title('naturalness')
-% grid on
-
-
-%
-%  Difference in objective measure when making correct or incorrect guess
-%
-%  For all the different speakers:
-%
-%  Similarity            Naturalness
-%  Roger: 
-%  
-%  2008{1,3}             2008{2,4}
-%  2009{1,3}             2009{2,4}
-%  2010{3}               2010{4}
-%
-%  Speakit:
-%  2008{5}               2008{6}
-%
-%  WJ:
-%  2009{5,7}             2009{6,8}
-%
-%  rjs:
-%  2010{1,5,7}           2010{2,6,8}
-%
-%  CAS speaker:
-%  2010{9,11,13}         2010{10,12,14}
-%
-%  Nancy:
-%  2011{1}               2011{2}
-%
-%  Librivox speaker:
-%  2012{1}               2012{2}
-%
-%  Voice Forge speaker:
-%  2013{1,3}             2013{2,4}
-
-
-%
-%  All systems:
-%
-
-comparisontypeslist={ {'Unit selection systems only', 1} ...
-                      {'HMM-systems only',2} ...
-                      {'All systems', 1:3 } };
-
-res = { struct('simnons',{{}},'simsigs',{{}},'simmins',[],'simmaxs',[],'simspeakers',{{}}, 'simperformances', {{}},'natnons',{{}},'natsigs',{{}},'natmins',[],'natmaxs',[],'natspeakers',{{}}, 'natperformances', {{}} ), ...
-        struct('simnons',{{}},'simsigs',{{}},'simmins',[],'simmaxs',[],'simspeakers',{{}}, 'simperformances', {{}},'natnons',{{}},'natsigs',{{}},'natmins',[],'natmaxs',[],'natspeakers',{{}}, 'natperformances', {{}} ), ...
-        struct('simnons',{{}},'simsigs',{{}},'simmins',[],'simmaxs',[],'simspeakers',{{}}, 'simperformances', {{}},'natnons',{{}},'natsigs',{{}},'natmins',[],'natmaxs',[],'natspeakers',{{}}, 'natperformances', {{}} ) };
-
-                  
-for comparisoncount=1:3                      
-
-    comparisontypes=comparisontypeslist{comparisoncount}{2};
-    comparisonname=comparisontypeslist{comparisoncount}{1};
-
-
-    n=0;
-    m=0;
-
-    for p=1:length(tests)
-        for r=1:length(tests{p})
-            t=tests{p}{r};
-
-            [sigs,nons]=get_significance_distances_by_systemtype(t.results, load(t.subjective_resultfile), load(t.opinionmatrix),t.systems, t.systemtypes);
-
-            for comparisontype=comparisontypes
-
-                if t.testtype == 'sim'
-                    n=n+1;
-                    res{comparisoncount}.simnons{n}=nons{comparisontype};
-                    res{comparisoncount}.simsigs{n}=sigs{comparisontype};
-                    res{comparisoncount}.simspeakers{n}=t.speaker;
-                else
-                    m=m+1;
-                    res{comparisoncount}.natnons{m}=nons{comparisontype};
-                    res{comparisoncount}.natsigs{m}=sigs{comparisontype};          
-                    res{comparisoncount}.natspeakers{m}=t.speaker;
-                end
-            end
-        end
-    end
-    
-    
-end
-
-for comparisoncount=1:3               
-    
-    % Mins and maxes for similarities
-    
-    simsigs=res{comparisoncount}.simsigs;
-    simnons=res{comparisoncount}.simnons;
-
-    mins=min(abs(simsigs{1}'));
-    maxs=max(abs(simsigs{1}'));
-
-    for n=2:length(simsigs)
-        if ~isempty(simsigs{n})         
-            mins=min([mins',abs(simsigs{n})]');
-            maxs=max([maxs',abs(simsigs{n})]');
-        end
-    end
-    
-    res{comparisoncount}.simmins=mins;
-    res{comparisoncount}.simmaxs=maxs;
-
-    
-    % Mins and maxes for naturalness
-    
-    
-    natsigs=res{comparisoncount}.natsigs;
-    natnons=res{comparisoncount}.natnons;
-
-    mins=min(abs(natsigs{1}'));
-    maxs=max(abs(natsigs{1}'));
-
-    for n=2:length(natsigs)
-        if ~isempty(natsigs{n})         
-            mins=min([mins',abs(natsigs{n})]');
-            maxs=max([maxs',abs(natsigs{n})]');
-        end
-    end
-    
-    res{comparisoncount}.natmins=mins;
-    res{comparisoncount}.natmaxs=maxs;   
-    
-    
-    res{comparisoncount}.natperformances=zeros(length(featlist),20);    
-    res{comparisoncount}.simperformances=zeros(length(featlist),20);    
-        
-end
-
-
-end
 
 %
 %
@@ -252,9 +23,19 @@ handbookcurves = cell(1,size(simsigs{1},1));
 
 %%%% For all feats! 
 
-for feat=1:size(simsigs{1},1);    
+for feat=[2,12,38,50] %1:size(simsigs{1},1);    
 
     clf
+
+    
+    
+    mins=res{1}.simmins;
+    maxs=res{2}.simmaxs;    
+
+    for comparisoncount=2:3     
+        mins=min(mins,res{comparisoncount}.simmins);
+        maxs=max(maxs,res{comparisoncount}.simmaxs);
+    end
     
     for comparisoncount=1:3                    
         
@@ -268,9 +49,6 @@ for feat=1:size(simsigs{1},1);
         if feat>20
             logbase=30;
         end
-        
-        mins=res{comparisoncount}.simmins;
-        maxs=res{comparisoncount}.simmaxs;
         
         binstepsize=(maxs(feat)-mins(feat))/binsteps;
         binstepsize=roundn(binstepsize,floor(log10(binstepsize)));
@@ -355,14 +133,15 @@ for feat=1:size(simsigs{1},1);
         srvals=sort(vals);
         srnonvals=sort(nonvals);
         
-        accum=zeros(sum(srvals>0),1);
+        %accum=zeros(sum(srvals>0),1);
+        accum=zeros(size(srvals));
         nonsaccum=zeros(sum(srvals>0),1);
         
-        ct=0;
+        %ct=0;
         for m=min(find(srvals>0)):length(srvals)
-            ct=ct+1;
-            accum(ct)=sum((srvals>srvals(m)))/(sum((srvals>srvals(m)))+sum((srvals<(-srvals(m)))));
-            nonsaccum(ct)=sum((srvals>srvals(m)))/(  sum(srvals>srvals(m)) + sum(srvals<(-srvals(m))) + sum(srnonvals<(-srvals(m))) + sum(srnonvals>srvals(m))  );
+            %ct=ct+1;
+            accum(m)=sum((srvals>srvals(m)))/(sum((srvals>srvals(m)))+sum((srvals<(-srvals(m)))));
+            nonsaccum(m)=sum((srvals>srvals(m)))/(  sum(srvals>srvals(m)) + sum(srvals<(-srvals(m))) + sum(srnonvals<(-srvals(m))) + sum(srnonvals>srvals(m))  );
         end
 
         invaccum=zeros(sum(srvals<0),1);  
@@ -461,7 +240,7 @@ for feat=1:size(simsigs{1},1);
         end
         xlabel('Value difference');
         ylabel('% correct significant evaluations');
-        %axis([0,max(),0,1]);
+        axis([0,maxs(feat),0,1]);
 
         grid minor
         title(['Similarity, ',comparisonname],'FontSize',18); 
@@ -473,6 +252,14 @@ for feat=1:size(simsigs{1},1);
     
     %%% NATURALNEESS!!!
     
+    mins=res{1}.natmins;
+    maxs=res{2}.natmaxs;    
+
+    for comparisoncount=2:3     
+        mins=min(mins,res{comparisoncount}.natmins);
+        maxs=max(maxs,res{comparisoncount}.natmaxs);
+    end
+        
   
     for comparisoncount=1:3                    
         
@@ -486,9 +273,6 @@ for feat=1:size(simsigs{1},1);
         if feat>20
             logbase=30;
         end
-        
-        mins=res{comparisoncount}.natmins;
-        maxs=res{comparisoncount}.natmaxs;
         
         binstepsize=(maxs(feat)-mins(feat))/binsteps;
         binstepsize=roundn(binstepsize,floor(log10(binstepsize)));
@@ -569,17 +353,18 @@ for feat=1:size(simsigs{1},1);
         
         handbookplottables{feat} = [sigsbinc;negsbinc;nonsbinc];
 
-          srvals=sort(vals);
+       	srvals=sort(vals);
         srnonvals=sort(nonvals);
         
-        accum=zeros(sum(srvals>0),1);
+        % accum=zeros(sum(srvals>0),1);
+        accum=zeros(size(srvals));
         nonsaccum=zeros(sum(srvals>0),1);
         
-        ct=0;
+        %ct=0;
         for m=min(find(srvals>0)):length(srvals)
-            ct=ct+1;
-            accum(ct)=sum((srvals>srvals(m)))/(sum((srvals>srvals(m)))+sum((srvals<(-srvals(m)))));
-            nonsaccum(ct)=sum((srvals>srvals(m)))/(  sum(srvals>srvals(m)) + sum(srvals<(-srvals(m))) + sum(srnonvals<(-srvals(m))) + sum(srnonvals>srvals(m))  );
+            %ct=ct+1;
+            accum(m)=sum((srvals>srvals(m)))/(sum((srvals>srvals(m)))+sum((srvals<(-srvals(m)))));
+            nonsaccum(m)=sum((srvals>srvals(m)))/(  sum(srvals>srvals(m)) + sum(srvals<(-srvals(m))) + sum(srnonvals<(-srvals(m))) + sum(srnonvals>srvals(m))  );
         end
 
         invaccum=zeros(sum(srvals<0),1);  
@@ -678,7 +463,7 @@ for feat=1:size(simsigs{1},1);
         end
         xlabel('Value difference');
         ylabel('% correct significant evaluations');
-        %axis([0,max(),0,1]);
+        axis([0,maxs(feat),0,1]);
 
         grid minor
         title(['Naturalness, ',comparisonname],'FontSize',18); 
