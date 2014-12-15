fc8bcafae97bb59fffe851691f491e5924ee8c9a
rkarhila <rkarhila@iki.fi>
2014-12-15 17:40:07 +0200

    what is this thing here?

--- scripts/get_significance_distances_by_systemtype.m
+++ scripts/get_significance_distances_by_systemtype.m
@@ -1,18 +1,16 @@
-function [significants_by_type, non_significants_by_type] = get_significance_distances_by_systemtype(objective_scores, subjective_scores, opinion_matrix, systems, systemtypes)
+function [significants_by_type, non_significants_by_type] = get_significance_distances_by_systemtype(testname,objective_scores, subjective_scores, opinion_matrix, systems, systemtypes)
 
 listeningmeans=subjective_scores;
 refmat=opinion_matrix;
 refscores=objective_scores;
 
-
-opinion_matrix
-
-invdiag=ones(size(refmat))-diag(ones(size(refmat,1),1));
+%opinion_matrix;
+%invdiag=ones(size(refmat))-diag(ones(size(refmat,1),1));
 
 syscount=length(systems);
 featcount=size(refscores,2);
 
-testlen=length(subjective_scores);
+%testlen=length(subjective_scores);
 
 testlen=size(objective_scores,1)/length(systems);
 
@@ -53,7 +51,7 @@ end
 
 
 
-typematrix=zeros(length(systemtypes))
+typematrix=zeros(length(systemtypes));
 for n=1:length(systemtypes)
     for m=1:n
         if systemtypes(n)=='c' && systemtypes(m)=='c' 
@@ -77,12 +75,21 @@ for n=1:3
     significant_distances=zeros(size(refscores,2), sum(sum(triu(refmat==1,1).*abs(sign(betters)).*(typematrix==n))));
     non_significant_distances=zeros(size(refscores,2), sum(sum(triu(refmat==0,1).*abs(sign(betters)).*(typematrix==n))));
     
-    for feat = 1:featcount        
+    for feat = 1:featcount     
+        
+        if sum((machinebetters{feat}+diag(ones(length(machinebetters{feat}),1)))==0)>0
+            disp(testname)
+%            disp(['Some machinebetters{feat} zero for feat ',num2str(feat)]);
+%            disp((machinebetters{feat}+diag(ones(length(machinebetters{feat}),1))))
+%            disp(machinemeans(:,feat))
+
+        end
+        
         machinebetters{feat}(machinebetters{feat}==0)=1e-30;
 
-        machinebetters{feat}
+        machinebetters{feat};
 
-        directionmatch = triu(sign(machinebetters{feat}).*sign(betters)) ;    
+        %directionmatch = triu(sign(machinebetters{feat}).*sign(betters)) ;    
 
         significant_diffs=triu(machinebetters{feat}.*sign(betters).*(refmat==1).*(typematrix==n),1);
         significant_diffs=significant_diffs(:)';
